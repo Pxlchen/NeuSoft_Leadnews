f0c37380bae24e9721a16435978def77ab68a184
HchenP <1107383160@qq.com>
2023-09-10 10:11:04 +0000

    删除文件 后端/neusoft-news-service

--- 后端/neusoft-news-service/neusoft-news-user/src/main/java/com/neusoft/user/mapper/ApUserFollowMapper.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.user.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.neusoft.model.user.pojos.ApUserFollow;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ApUserFollowMapper extends BaseMapper<ApUserFollow> {
-}
--- 后端/neusoft-news-service/neusoft-news-user/src/main/java/com/neusoft/user/mapper/ApUserRealnameMapper.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.user.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.neusoft.model.user.pojos.ApUserRealname;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ApUserRealnameMapper extends BaseMapper<ApUserRealname> {
-}
