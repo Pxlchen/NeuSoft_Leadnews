5125989b7efdbd1230f89f9e51dc2cee32adcbd5
zslrj <2949888732@qq.com>
2023-09-10 19:23:59 +0800

    搜索模块修改

--- 后端/Neusoft-news/neusoft-news-model/src/main/java/com/neusoft/model/search/dtos/UserSearchDto.java
+++ 后端/Neusoft-news/neusoft-news-model/src/main/java/com/neusoft/model/search/dtos/UserSearchDto.java
@@ -1,5 +1,6 @@
 package com.neusoft.model.search.dtos;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -11,18 +12,22 @@ public class UserSearchDto {
     /**
     * 搜索关键字
     */
+    @ApiModelProperty(value = "搜索关键字", required = true)
     String searchWords;
     /**
     * 当前页
     */
+    @ApiModelProperty(value = "当前页", required = true)
     int pageNum;
     /**
     * 分页条数
     */
+    @ApiModelProperty(value = "分页条数", required = true)
     int pageSize;
     /**
     * 最小时间
     */
+    @ApiModelProperty(value = "最小时间")
     Date minBehotTime;
 
     public int getFromIndex(){
--- 后端/Neusoft-news/neusoft-news-service/neusoft-news-search/src/main/java/com/neusoft/search/controller/v1/ApAssociateWordsController.java
+++ 后端/Neusoft-news/neusoft-news-service/neusoft-news-search/src/main/java/com/neusoft/search/controller/v1/ApAssociateWordsController.java
@@ -3,6 +3,8 @@ package com.neusoft.search.controller.v1;
 import com.neusoft.model.common.dtos.ResponseResult;
 import com.neusoft.model.search.dtos.UserSearchDto;
 import com.neusoft.search.service.ApAssociateWordsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/associate")
+@Api(value = "app端用户搜索关键词联想词", tags = "app端用户搜索关键词联想词")
 public class ApAssociateWordsController {
 
     @Autowired
     private ApAssociateWordsService apAssociateWordsService;
 
+    /**
+     * 搜索联想词
+     *
+     * @param dto
+     * @return
+     */
     @PostMapping("/search")
-    public ResponseResult search(@RequestBody UserSearchDto dto){
+    @ApiOperation("搜索联想词")
+    public ResponseResult search(@RequestBody UserSearchDto dto) {
         return apAssociateWordsService.search(dto);
     }
 }
